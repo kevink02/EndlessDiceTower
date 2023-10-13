ef949f536f854fdca5ee3c11d1d7169f86937e4f
kevink02 <kevinkubota0@outlook.com>
2023-10-12 18:37:21 -0700

    Added interface

---
+++ Assets/Scripts/Interfaces/IRollable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IRollable
+{
+    public int GetRolledValue();
+}
--- Assets/Scripts/RollableDie.cs
+++ Assets/Scripts/RollableDie.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "New RollableDie")]
-public class RollableDie : ScriptableObject
+public class RollableDie : ScriptableObject, IRollable
 {
     // Replace this variable type later
     [SerializeField]
     protected string AttackAnimation;
 
-    private int _rolledValue;
+
+    /*
+     * Interface methods
+     */
+    public int GetRolledValue()
+    {
+        return Random.Range(1, 7);
+    }
 }
