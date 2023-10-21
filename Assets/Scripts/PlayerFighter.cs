91da0bc4fc3cb412e6578c5e9bbee99fea153493
kevink02 <kevinkubota0@outlook.com>
2023-10-20 17:08:13 -0700

    Update PlayerFighter.cs

--- Assets/Scripts/PlayerFighter.cs
+++ Assets/Scripts/PlayerFighter.cs
@@ -23,12 +23,25 @@ public class PlayerFighter : DiceFighter
 
     private void OnEnable()
     {
-        _playerInputActions.DiceRolling.RollDice.Enable();
+        _playerInputActions.DiceRolling.RollDie1.Enable();
+        _playerInputActions.DiceRolling.RollDie2.Enable();
+        _playerInputActions.DiceRolling.RollDie3.Enable();
     }
 
     private void Start()
     {
-        _playerInputActions.DiceRolling.RollDice.performed += cxt => RollDie(cxt);
+        // _playerInputActions.DiceRolling.RollDie1.performed += cxt => RollDie(cxt);
+        // Debug.Log($"keyBindingsArr: {_playerInputActions.DiceRolling.RollDice.bindings}");
+        _playerInputActions.DiceRolling.RollDie1.performed += cxt => RollDie(0);
+        _playerInputActions.DiceRolling.RollDie2.performed += cxt => RollDie(1);
+        _playerInputActions.DiceRolling.RollDie3.performed += cxt => RollDie(2);
+    }
+
+    private void OnDisable()
+    {
+        _playerInputActions.DiceRolling.RollDie1.Disable();
+        _playerInputActions.DiceRolling.RollDie2.Disable();
+        _playerInputActions.DiceRolling.RollDie3.Disable();
     }
 
 
@@ -40,9 +53,26 @@ public class PlayerFighter : DiceFighter
 
     }
 
+    private void RollDie(int index)
+    {
+        Debug.Log($"{name}: Rolling die #{index}");
+    }
+
     private void RollDie(InputAction.CallbackContext context)
     {
         Debug.Log($"key: {context}");
-        Debug.Log($"key: {context.ReadValue<float>()}");
+        Debug.Log($"keyControl: {context.control}");
+        // Debug.Log($"keyValue: {context.ReadValue<float>()}");
+
+        // Does work in checking if a specific key is pressed, but not flexible if wanting to rebind keys
+        // Debug.Log($"keyboardPress: {Keyboard.current.digit1Key.isPressed}");
+
+        var arr = context.action.bindings;
+        int index = 0;
+        foreach (var v in arr)
+        {
+            Debug.Log($"keyInArr: {v}");
+            index++;
+        }
     }
 }
