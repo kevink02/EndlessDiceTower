ea1af56cb5783a9159c7b79e7388c53c1a9b21fa
kevink02 <kevinkubota0@outlook.com>
2023-10-23 01:48:42 -0700

    Created manager classes

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    /*
+     * Unity methods
+     */
+    private void Awake()
+    {
+        
+    }
+}
