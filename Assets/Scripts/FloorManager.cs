5a2d5d54426abb8d944495d8bd610e22c78ac39f
kevink02 <kevinkubota0@outlook.com>
2023-10-23 02:13:57 -0700

    Updated manager classes

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorManager : MonoBehaviour
+public class FloorManager : IManager
 {
+    /*
+     * Instance variables
+     */
+    private int _currentFloor;
+    [SerializeField]
+    private TurnManager _turnManager;
+
+
     /*
      * Unity methods
      */
     private void Awake()
     {
-        
+        _currentFloor = 0;
+        _turnManager.ResetState();
+    }
+
+
+    /*
+     * Interface methods
+     */
+    public void ExecuteState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void NextState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResetState()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -2,13 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnManager : MonoBehaviour
+public class TurnManager : IManager
 {
+    /*
+     * Instance variables
+     */
+    private Queue<DiceFighter> _fighterTurnQueue;
+
+
     /*
      * Unity methods
      */
     private void Awake()
     {
-        
+        _fighterTurnQueue = new Queue<DiceFighter>();
+    }
+
+
+    /*
+     * Interface methods
+     */
+    public void ExecuteState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void NextState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResetState()
+    {
+        throw new System.NotImplementedException();
     }
 }
