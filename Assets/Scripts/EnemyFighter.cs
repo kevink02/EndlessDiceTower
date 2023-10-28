faa0a9a616e1d0de170f4c91e43dd4e1211e43ab
kevink02 <kevinkubota0@outlook.com>
2023-10-28 16:15:08 -0700

    Created enemy fighter class

---
+++ Assets/Scripts/EnemyFighter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFighter : DiceFighter
+{
+    /*
+     * Unity methods
+     */
+    private new void Awake()
+    {
+        base.Awake();
+    }
+
+    private new void Start()
+    {
+        base.Start();
+    }
+}
