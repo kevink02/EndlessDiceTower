47b11030b99d65c1f11e1f38f8aad96d7be6aa25
kevink02 <kevinkubota0@outlook.com>
2023-10-29 21:14:18 -0700

    Update FighterGenerator.cs

--- Assets/Scripts/FighterGenerator.cs
+++ Assets/Scripts/FighterGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,55 @@ using UnityEngine;
 public class FighterGenerator : BasicSingleton<FighterGenerator>
 {
     /*
+     * Properties
+     */
+    public List<EnemyFighter> EnemyFighters { get; private set; }
+    public List<PlayerFighter> PlayerFighters { get; private set; }
+
+
+    /*
+     * Delegates
+     */
+    private event Action CreateFighters;
+
+
+    /*
      * Unity methods
      */
     private void Awake()
     {
-        
+        EnemyFighters = new List<EnemyFighter>();
+        PlayerFighters = new List<PlayerFighter>();
+    }
+
+    private void OnEnable()
+    {
+        CreateFighters += CreatePlayerFighters;
+        CreateFighters += CreateEnemyFighters;
+    }
+
+    private void Start()
+    {
+        CreateFighters?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        CreateFighters -= CreatePlayerFighters;
+        CreateFighters -= CreateEnemyFighters;
+    }
+
+
+    /*
+     * Instance methods
+     */
+    private void CreatePlayerFighters()
+    {
+        Debug.Log("${name}: Creating player fighters");
+    }
+
+    private void CreateEnemyFighters()
+    {
+        Debug.Log("${name}: Creating enemy fighters");
     }
 }
