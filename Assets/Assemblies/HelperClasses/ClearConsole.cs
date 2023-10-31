37fba4ab191d223536f724889aa5826196f2be83
kevink02 <kevinkubota0@outlook.com>
2023-10-30 20:57:02 -0700

    Created console clearing class

---
+++ Assets/Assemblies/HelperClasses/ClearConsole.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// Clears the console log
+/// </summary>
+public static class ClearConsole
+{
+    public static void ClearLog()
+    {
+        var assembly = System.Reflection.Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+}
