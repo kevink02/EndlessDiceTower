2bdc05ecf550ccab9161ddc62d5407a196965c57
kevink02 <kevinkubota0@outlook.com>
2023-11-08 20:07:53 -0800

    Created element type class

---
+++ Assets/Assemblies/MainClasses/ElementType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Element Type")]
+public class ElementType : ScriptableObject
+{
+    // This will be used instead of an enum for element types
+    // No variables or methods are needed as of now
+}
