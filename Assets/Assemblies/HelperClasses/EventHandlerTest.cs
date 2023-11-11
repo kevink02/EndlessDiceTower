ce8d2b8be02c47ad223563f05773493f6863e7e4
kevink02 <kevinkubota0@outlook.com>
2023-11-11 02:02:31 -0800

    Created class for testing event handlers

---
+++ Assets/Assemblies/HelperClasses/EventHandlerTest.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+
+public class EventHandlerTest : MonoBehaviour
+{
+    public event EventHandler<EventArgs> OnEventTest;
+
+    public void Awake()
+    {
+    }
+
+    public void OnEnable()
+    {
+        OnEventTest += Test;
+        OnEventTest += TestA;
+        OnEventTest += TestB;
+    }
+
+    public void Start()
+    {
+        OnEventTest(this, new EventArgsB());
+    }
+
+    public void OnDisable()
+    {
+        OnEventTest -= Test;
+        OnEventTest -= TestA;
+        OnEventTest -= TestB;
+    }
+
+    public void Test(object sender, EventArgs args)
+    {
+        args.TestVar += "?";
+        Debug.Log($"Argument value is now {args.TestVar}");
+    }
+
+    public void TestA(object sender, EventArgs args)
+    {
+        Debug.Log($"Argument value is {args.TestVar}");
+        if (args is EventArgsA a)
+        {
+            a.TestVarA += 1;
+            Debug.Log($"Argument A value is now {a.TestVarA}");
+        }
+    }
+
+    public void TestB(object sender, EventArgs args)
+    {
+        Debug.Log($"Argument value is {args.TestVar}");
+        if (args is EventArgsB b)
+        {
+            b.TestVarB += 0.1f;
+            Debug.Log($"Argument B value is now {b.TestVarB}");
+        }
+    }
+}
+
+public abstract class EventArgs
+{
+    public string TestVar;
+}
+
+public class EventArgsA : EventArgs
+{
+    public int TestVarA;
+}
+
+public class EventArgsB : EventArgs
+{
+    public float TestVarB;
+}
