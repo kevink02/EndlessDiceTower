6d0eeb5849cdf1bd586cfbcafe3b8ffd41f9e2c4
kevink02 <kevinkubota0@outlook.com>
2023-11-16 02:10:29 -0800

    Update SceneSwapper.cs

--- Assets/Assemblies/MainClasses/SceneSwapper.cs
+++ Assets/Assemblies/MainClasses/SceneSwapper.cs
@@ -12,13 +12,27 @@ public class SceneSwapper : BasicSingleton<SceneSwapper>
 
 
     /*
-     * Instance methods
+     * Unity methods
      */
-    public void SwitchScene(GameObject sceneToSwitchTo)
+    private void Awake()
     {
-        Debug.Log($"{name}: Opening {sceneToSwitchTo}'s scene");
+        if (_startScene == null || _levelScene == null)
+        {
+            throw new System.NullReferenceException("One of the scenes is not set");
+        }
     }
 
+
+    /*
+     * Instance methods
+     */
+    // Used by some UI buttons for switching scenes
+    public void SwitchScene(GameScene sceneToSwitchTo)
+    {
+        sceneToSwitchTo.SwitchScene();
+    }
+
+    // Used by UI quit button to quit the game
     public void QuitGame()
     {
         Debug.LogWarning($"{name}: Quitting the game...");
