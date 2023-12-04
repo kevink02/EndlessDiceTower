07d18443ece4539208a85bdf3c05ba3c5eaa3a10
kevink02 <kevinkubota0@outlook.com>
2023-12-04 14:12:44 -0800

    Added comments

--- Assets/Assemblies/HelperClasses/TestSOMethods.cs
+++ Assets/Assemblies/HelperClasses/TestSOMethods.cs
@@ -6,18 +6,28 @@ public class TestSOMethods : ScriptableObject
     /*
      * Unity methods
      */
+    // Gets called once upon the SO asset's creation
+    // Doesn't seem to be called anymore during runtime
     private void Awake()
     {
         Debug.Log("SO's Awake was called");
     }
 
+    // Works upon asset creation and at runtime
+    // Doesn't seem to be called if the object it is embedded in gets disabled and re-enabled
     private void OnEnable()
     {
         Debug.Log("SO's OnEnable was called");
     }
 
+    // Works upon asset creation and at runtime
+    // Runs before OnEnable()
     private void OnValidate()
     {
         Debug.Log("SO's OnValidate was called");
     }
+
+    // As of now, I do not plan on destroying SO assets
+    // So, do not need to worry about OnDestroy() for now
+    // Maybe need to look at OnDisable()
 }
